e46af4a3547881609adec38df16715070c171b53
Jenna <jenna@example.com>
2025-03-10 11:33:43 +0100

    MAJ

---
+++ javafx-app/src/main/java/com/example/FXMLApp.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FXMLApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Charger l'interface FXML
+        VBox root = FXMLLoader.load(getClass().getResource("/interface.fxml"));
+
+        // Créer la scène et afficher la fenêtre
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("Interface FXML");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
